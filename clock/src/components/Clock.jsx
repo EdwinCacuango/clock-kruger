90100380c54638dcb3b5a71ec4317a16648b8742
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-12-27 23:28:14 -0500

    Styles for numbers inside clock - Add a span tag in every div with hour number to control better their     rotation - Add style for this additional span to align the number

--- clock/src/components/Clock.jsx
+++ clock/src/components/Clock.jsx
@@ -4,18 +4,42 @@ const Clock = () => {
       <div className="hand hour" data-hour-hand></div>
       <div className="hand minute" data-minute-hand></div>
       <div className="hand second" data-second-hand></div>
-      <div className="number number1">1</div>
-      <div className="number number2">2</div>
-      <div className="number number3">3</div>
-      <div className="number number4">4</div>
-      <div className="number number5">5</div>
-      <div className="number number6">6</div>
-      <div className="number number7">7</div>
-      <div className="number number8">8</div>
-      <div className="number number9">9</div>
-      <div className="number number10">10</div>
-      <div className="number number11">11</div>
-      <div className="number number12">12</div>
+      <div className="number number1">
+        <span>1</span>
+      </div>
+      <div className="number number2">
+        <span>2</span>
+      </div>
+      <div className="number number3">
+        <span>3</span>
+      </div>
+      <div className="number number4">
+        <span>4</span>
+      </div>
+      <div className="number number5">
+        <span>5</span>
+      </div>
+      <div className="number number6">
+        <span>6</span>
+      </div>
+      <div className="number number7">
+        <span>7</span>
+      </div>
+      <div className="number number8">
+        <span>8</span>
+      </div>
+      <div className="number number9">
+        <span>9</span>
+      </div>
+      <div className="number number10">
+        <span>10</span>
+      </div>
+      <div className="number number11">
+        <span>11</span>
+      </div>
+      <div className="number number12">
+        <span>12</span>
+      </div>
     </div>
   );
 };
